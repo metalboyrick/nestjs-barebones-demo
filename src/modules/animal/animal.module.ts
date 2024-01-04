bdd1268a63c5b59ce1f8c2489fd0f63edba2dd51
metalboyrick <rsulisthio@gmail.com>
2024-01-04 21:48:21 +0700

    feat: add sample middleware

--- src/modules/animal/animal.module.ts
+++ src/modules/animal/animal.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AnimalController } from './animal.controller';
 import { AnimalService } from './animal.service';
 import { AnimalDao } from './animal.dao';
+import { LoggerMiddleware } from 'src/middlewares/logger';
 
 @Module({
   controllers: [AnimalController],
   providers: [AnimalService, AnimalDao],
 })
-export class AnimalModule {}
+export class AnimalModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('animal');
+  }
+}
